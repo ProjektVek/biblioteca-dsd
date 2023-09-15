1a5c769a825be1899e21639fd73e6266a1c5b2c6
Victor Hugo "VeK" Ferreira <ProjektVek@gmail.com>
2023-09-15 12:03:48 -0300

    Books page added

--- src/php/login.php
+++ src/php/login.php
@@ -8,7 +8,7 @@
     $loginResult = checkLogin($username, $password);
     if($loginResult){
         $_SESSION['username'] = $username;
-        header('Location: /src/books.html'); exit; 
+        header('Location: /src/books.php'); exit; 
     } else {
         echo "<script> alert('Usuário ou senha inválida') </script>";
         header('Location: /src/loginPage.php?login=invalid'); exit; 
