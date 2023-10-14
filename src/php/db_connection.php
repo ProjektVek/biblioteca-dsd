3faca205ca81c96e55bdb9a618386385a2804e0e
Victor Hugo "VeK" Ferreira <ProjektVek@gmail.com>
2023-10-14 13:50:24 -0300

    Updated database connection

--- src/php/db_connection.php
+++ src/php/db_connection.php
@@ -1,16 +1,35 @@
 <?php
 function openConnection()
 {
-    $db_host = 'database';
+    $db_host_original = 'database';
+    $db_host_replica = 'localhost';
     $db_user = 'dsdcc8';
     $db_password = 'cc8@unip';
     $db_name = 'Biblioteca';
 
-    $connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+    try{
+        $connection = mysqli_connect($db_host_original, $db_user, $db_password, $db_name);
+    } catch (Exception $e) {
+        error_log("Exception caught: " . $e->getMessage());
+    }
+    
 
     if (!$connection) {
-        echo ("Connection failed: " . mysqli_connect_error());
-        return $connection . mysqli_connect_error();
+        error_log ("Connection with original failed: " . mysqli_connect_error());
+
+        try {
+            $connection = mysqli_connect($db_host_replica, $db_user, $db_password, $db_name);
+        } catch (Exception $e) {
+            error_log("Exception caught: " . $e->getMessage());
+        }
+        
+
+        if (!$connection) {
+            error_log("Connection with replica failed: " . mysqli_connect_error());
+            return $connection . mysqli_connect_error();
+        } else {
+            return $connection;
+        }
     } else {
         return $connection;
     }
